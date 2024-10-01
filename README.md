fe80ded18eca8db875f1905b2c7f06d38cc25b74
Ilia Mistiurin <mistyurin.ilya.im@gmail.com>
2024-10-02 00:29:08 +0300

    Adding 'api + webapp + mlflow' deployment

--- README.md
+++ README.md
@@ -17,3 +17,10 @@ cd code/deployment
 
 docker-compose up --build
 ```
+
+Generating dockerfile for model serving
+
+```bash
+export MLFLOW_TRACKING_URI=http://localhost:8090
+mlflow models generate-dockerfile --model-uri "models/basic_model@production" --env-manager virtualenv -d mlflow_api --install-mlflow
+```
--- code/deployment/api/app.py
+++ code/deployment/api/app.py
@@ -1,9 +1,7 @@
-import datetime
+import json
 import logging
 import os
-import pickle
 
-import mlflow.pytorch
 import numpy as np
 import pandas as pd
 import torch
@@ -12,20 +10,17 @@ from fastapi import FastAPI, Request
 from fastapi.concurrency import asynccontextmanager
 from pydantic_models import StudentDataModel
 
+import requests
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 load_dotenv()
 
-MODEL_NAME = os.getenv("MODEL_NAME", "best")
-MODEL_VERSION = os.getenv("MODEL_VERSION", "1")
-
-model_uri = f"models/{MODEL_NAME}/{MODEL_VERSION}"
-model = mlflow.pytorch.load_model(model_uri)
-
-# with open("models/best.pkl", "rb") as f:
-#     model = pickle.load(f)
+MODEL_NAME = os.getenv("MODEL_NAME", "basic_model")
+MODEL_ALIAS = os.getenv("MODEL_ALIAS", "production")
+MLFLOW_URL = os.getenv("MLFLOW_URL", "http://mlflow:8090")
 
 
 @asynccontextmanager
@@ -33,10 +28,7 @@ async def lifespan(app: FastAPI):
     try:
         global model, preprocesser
         logger.info("Starting.")
-
-        logger.info("Loading model.")
-
-        logger.info("Model has been loaded")
+        logger.info(f"Using MLflow model: {MODEL_NAME}@{MODEL_ALIAS}")
     except Exception as e:
         logger.error(f"Error during starting {e}.")
 
@@ -49,12 +41,313 @@ app = FastAPI(lifespan=lifespan)
 
 @app.post("/main")
 async def main(request: Request, data: StudentDataModel) -> None:
-    logger.info(f"Received data: {data}")
+    logger.info(f"\n\nReceived data: {data}\n\n")
 
-    input_data = data.model_dump()
+    # input_data = data.model_dump()
 
-    prediction = model(torch.tensor(input_data.values, dtype=torch.float32))
-    ans = preprocesser.reverse_label_transform(prediction.tolist())
+    input_payload = {
+        "inputs": [
+            [
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                1.0,
+                0.0,
+                0.0,
+                0.0,
+                1.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                1.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                1.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                1.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                1.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                1.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                1.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                1.0,
+                0.0,
+                0.0,
+                0.14744850993156433,
+                -0.3712688982486725,
+                -0.6210671663284302,
+                -0.0816706046462059,
+                -0.16070155799388885,
+                0.06489204615354538,
+                0.7546852231025696,
+                0.6776396632194519,
+                0.5706744194030762,
+                -0.14188984036445618,
+                -0.1467650979757309,
+                0.04092109203338623,
+                0.21853511035442352,
+                0.3580314815044403,
+                0.6804752349853516,
+                -0.1351272612810135,
+                0.8968483805656433,
+                -1.092515230178833,
+                0.3868410587310791,
+            ]
+        ]
+    }
 
-    print(type(ans))
-    return {"prediction": ans.tolist()}
+    prediction = None
+
+    try:
+        response = requests.post(
+            f"{MLFLOW_URL}/invocations",
+            json=input_payload,
+        )
+
+        if response.status_code == 200:
+            prediction = response.json()
+            logger.info("Prediction:", prediction)
+        else:
+            logger.warning(f"Error: Received status code {response.status_code}")
+            logger.warning("Response content:", response.text)
+
+    except Exception as e:
+        logger.error(f"An error occurred while making the request: {e}")
+
+    return {"prediction": prediction}
