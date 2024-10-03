c4ddc5faa244a34ec6c33f644d80d4c3da3aa90c
Ilia Mistiurin <mistyurin.ilya.im@gmail.com>
2024-10-03 23:35:28 +0300

    without full automation

--- README.md
+++ README.md
@@ -2,25 +2,26 @@
 
 For "Practical Machine Learning and Deep Learning" Innopolis University course.
 
-To run airflow:
+To run minimal deployment: FastAPI + Streamlit run:
+
+```bash
+docker-compose -f code/deployment/deployment.compose.yaml up --build
+```
+
+Generating dockerfile for model serving (check )
+
+```bash
+mlflow server -h localhost -p 8090 &
+export MLFLOW_TRACKING_URI=http://localhost:8090
+cd  code/deployment/models
+python training.py
+mlflow models generate-dockerfile --model-uri "models:/BasicModel@Champion" -d ./mlflow_api --install-mlflow
+docker build -t mls mlflow_api
+```
+
+To run airflow (zenml located in DAGs):
 
 ```bash
 cd services/airflow
-
 docker-compose -f advanced_air.docker-compose.yaml up --build
-```
-
-To run FastAPI + Streamlit run:
-
-```bash
-cd code/deployment
-
-docker-compose up --build
-```
-
-Generating dockerfile for model serving
-
-```bash
-export MLFLOW_TRACKING_URI=http://localhost:8090
-mlflow models generate-dockerfile --model-uri "models/basic_model@production" --env-manager virtualenv -d mlflow_api --install-mlflow
 ```
--- services/airflow/dags/servers_dag.py
+++ services/airflow/dags/servers_dag.py
@@ -1,4 +1,3 @@
-from time import sleep
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
@@ -15,7 +14,7 @@ with DAG(
     "run_local_zenml",
     default_args=default_args,
     description="Run ZenML pipeline",
-    schedule_interval='*/5 * * * *',
+    schedule_interval="*/5 * * * *",
     catchup=False,
     tags=["tests"],
 ) as run_local_zenml:
@@ -45,9 +44,7 @@ with DAG(
         task_id="trigger_training",
         trigger_dag_id="run_training",
     )
-
     run_pipe >> trigger_training
-
 
 with DAG(
     "docker_compose_deploy_up",
@@ -101,18 +98,15 @@ with DAG(
 ) as run_training:
     training = BashOperator(
         task_id="run_training",
-        bash_command="""
-            set -e; \
-            bash /opt/code/models/init.sh
-            """,
+        bash_command="sh -c 'bash /opt/code/models/init.sh'",
         dag=run_training,
     )
     trigger_packing = TriggerDagRunOperator(
         task_id="trigger_packing",
         trigger_dag_id="run_packing",
     )
-
     training >> trigger_packing
+
 
 with DAG(
     "run_packing",
@@ -137,5 +131,72 @@ with DAG(
         task_id="trigger_deploy_up",
         trigger_dag_id="docker_compose_deploy_up",
     )
-
     packing >> trigger_deploy_up
+
+with DAG(
+    "unified_pipeline_dag",
+    default_args=default_args,
+    description="Unified DAG",
+    schedule_interval="*/5 * * * *",
+    catchup=False,
+    tags=["tests"],
+) as dag:
+    run_pipe = BashOperator(
+        task_id="run_pipeline",
+        bash_command="""
+            if ! ls /opt/data | grep -q processed; then \
+                mkdir /opt/data/processed; \
+            fi && \
+            source /opt/services/zenml/venv/bin/activate && \
+            cd /opt/services/zenml && \
+            zenml init && \
+            zenml down && \
+            zenml up && \
+            if ! zenml artifact-store list | grep -q artifacts; then \
+                zenml artifact-store register artifacts --flavor=local; \
+            fi && \
+            if ! zenml stack list | grep -q dev_stack; then \
+                zenml stack register dev_stack -o default -a artifacts; \
+            fi && \
+            zenml stack set dev_stack && \
+            python /opt/services/zenml/pipelines/training_pipeline.py -training_pipeline && \
+            zenml down
+        """,
+    )
+
+    run_training = BashOperator(
+        task_id="run_training",
+        bash_command="""bash /opt/code/models/init.sh""",
+    )
+
+    run_packing = BashOperator(
+        task_id="run_packing",
+        bash_command="""
+            cd /opt/code/models && \
+            docker build -t mls mlflow_api
+        """,
+    )
+
+    docker_compose_up = BashOperator(
+        task_id="docker_compose_up",
+        bash_command="""docker-compose -f /opt/code/deployment/deployment.compose.yaml up -d""",
+    )
+
+    wait_for_10_minutes = BashOperator(
+        task_id="wait_for_10_minutes",
+        bash_command="""sleep 600""",
+    )
+
+    docker_compose_down = BashOperator(
+        task_id="docker_compose_down",
+        bash_command="""docker-compose -f /opt/code/deployment/deployment.compose.yaml down""",
+    )
+
+    (
+        run_pipe
+        >> run_training
+        >> run_packing
+        >> docker_compose_up
+        >> wait_for_10_minutes
+        >> docker_compose_down
+    )
